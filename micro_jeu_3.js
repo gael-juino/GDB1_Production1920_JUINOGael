6727059a7be3608fbec5fc1ee274630e05bfda46
gael-juino <gael.juino29@gmail.com>
2020-06-10 17:22:27 +0200

    Scene3
    début de la scene 3
    ajout de teewns
    double saut

--- micro_jeu_3.js
+++ micro_jeu_3.js
@@ -7,22 +7,50 @@ class Scene3 extends Phaser.Scene {
 	this.load.image('fond2','assets/sanctuaire.png');
 	this.load.image('timebar','assets/timebar.png');
 	this.load.image('caisse','assets/caisse.png');
+	this.load.image('caisse2','assets/caisse2.png');
+	this.load.image('platforms','assets/platforms.png');
+	this.load.image('samurai','assets/samurai.png');
+
 
 	}
 	create(){
+
+	//saut//
+	this.saut = 2;
+	this.nsaut = 1;
 	//fond//
 	this.add.image(360,265, 'fond2');
 
 	//cursors//
 	this.cursors = this.input.keyboard.createCursorKeys();
 
+	//platforms//
+	this.platforms = this.physics.add.staticGroup();
+	this.platforms.create(400, 528, 'platforms');
+	this.platforms.setVisible(false);
+
+	//anims perso//
+	this.samurai = this.physics.add.image(200,420 ,'samurai');
+	this.samurai.direction = 'right';
+	this.samurai.setBounce(0.02);
+	this.samurai.body.setGravityY(10);
+	this.samurai.setCollideWorldBounds(true);
+	this.physics.add.collider(this.samurai, this.platforms);
+	this.samurai.setVisible(true);
+
 	//caisse//
-	this.caisse = this.physics.add.image(200,420 ,'caisse');
-	this.paniette.direction = 'right';
-	caisse.setCollideWorldBounds(true);
-	caisse.body.setGravityY(-300);
-	this.physics.add.collider(caisse, platforms);
-	this.physics.add.overlap(player, caisse, hitsam, null, this);
+	this.caisse = this.physics.add.image(800,400,'caisse');
+	this.caisse.direction = 'right';
+	this.caisse.setCollideWorldBounds(true);
+	this.physics.add.collider(this.caisse, this.platforms);
+	this.physics.add.overlap(this.samurai, this.caisse);
+
+	//caisse2//
+	this.caisse2 = this.physics.add.image(800,400,'caisse2');
+	this.caisse2.direction = 'right';
+	this.caisse2.setCollideWorldBounds(true);
+	this.physics.add.collider(this.caisse2, this.platforms);
+	this.physics.add.overlap(this.samurai, this.caisse);
 
 
 	//timebar//
@@ -47,20 +75,65 @@ class Scene3 extends Phaser.Scene {
                 loop: true
             });
             this.gameTimer.paused = false;
+
+            this.cursors = this.input.keyboard.createCursorKeys();
 	}
+
+
 	update(){
 		// Deplacement du perso// 
 
-		if (this.cursors.left.isDown){
-        this.paniette.setVelocityX(-800);
-        }
-        else if (this.cursors.right.isDown){
-        this.paniette.setVelocityX(800);
+		if(this.cursors.up.isDown && this.samurai.body.touching.down){
+		this.saut = 2;
+		}
 
-        }
-        else{
-        this.paniette.setVelocityX(0);
-        }
+		if ((this.nSaut==1) && this.saut>0 && this.cursors.up.isDown){
+		this.saut --;
+		this.nSaut=0;
+
+		if (this.saut == 1) {
+		this.samurai.setVelocityY(-250);
+
+		if (this.samurai.body.velocity.y<0) {
+
+			}
+		}
+
+		if (this.saut == 0) {
+		this.samurai.setVelocityY(-250);
+		if (this.samurai.body.velocity.y<0) {
+		}}
+		}
+
+		if (this.cursors.up.isUp) {
+		this.nSaut=1;
+		}
+
+        //caisse//
+        if (this.caisse.x >= 800) {
+			this.tweens.add({
+				targets: this.caisse,
+				x:10,
+				ease: 'Linear',
+				duration: 4000,
+				repeat: -1
+			});
+		}
+
+		//caisse2//
+        if (this.caisse2.x >= 1000) {
+			this.tweens.add({
+				targets: this.caisse2,
+				x:10,
+				ease: 'Linear',
+				duration: 4000,
+				repeat: -1
+			});
+		}
+
+
+
+		
 
 	}
 }
